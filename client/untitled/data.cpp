aa725f6dec1dbdbfa2451b1e51f6d56f0545ddd8
VladimirBalun <vladimirbalun@yandex.ru>
2021-10-12 21:33:58 +0300

    refactor: remove unused code

--- client/untitled/data.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Владимир Балун on 08.10.2021.
-//
-
-#ifndef UNTITLED_DATA_CUH
-#define UNTITLED_DATA_CUH
-
-#endif //UNTITLED_DATA_CUH
--- client/untitled/data.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Владимир Балун on 08.10.2021.
-//
-
-#ifndef UNTITLED_DATA_H
-#define UNTITLED_DATA_H
-
-#endif //UNTITLED_DATA_H
